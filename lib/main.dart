b24566861fecbd4a6e5722cf34b1eddaab2481aa
unknown <esentur.ch@mail.ru>
2023-05-30 14:47:30 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,334 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyWidget());
+// }
+
+// class MyWidget extends StatelessWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         theme: ThemeData(
+//           appBarTheme: const AppBarTheme(
+//             color: Colors.green,
+//           ),
+//         ),
+//         title: "test app",
+//         home: Scaffold(
+//           appBar: AppBar(
+//               backgroundColor: Colors.blue,
+//               // leading: const Icon(Icons.ios_share_outlined),
+//               // actions: const [Icon(Icons.control_camera_rounded)],
+
+//               title: const Center(child: Text("My First App"))),
+//           body: Center(
+//               child: Container(
+//             width: 300,
+//             height: 300,
+//             decoration: BoxDecoration(
+//               borderRadius: BorderRadius.circular(150),
+//               color: Colors.red,
+//               border: Border.all(
+//                 color: Colors.blue,
+//                 width: 40,
+//               ),
+//             ),
+
+//             margin: const EdgeInsets.only(top: 30),
+//             padding: const EdgeInsets.only(top: 100),
+
+//             // constraints: const BoxConstraints(
+//             //   minHeight: 30,
+//             //   minWidth: 30,
+//             //   maxHeight: 50,
+//             //   maxWidth: 50,
+//             // ),
+//             //alignment: Alignment.topRight,
+//             // color: Colors.amber,
+//             child: const Text(
+//               "ITC BOOTCAMP",
+//               textAlign: TextAlign.center,
+//               style: TextStyle(
+//                 color: Colors.black,
+//                 fontSize: 20,
+//                 // fontWeight: FontWeight.w600),
+//               ),
+//             ),
+//           )),
+//         ));
+//   }
+// }
+
+////////////////////////222222222222222222222//////////////////////
+///
+///
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyWidget());
+// }
+
+// class MyWidget extends StatelessWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         theme: ThemeData(
+//           appBarTheme: const AppBarTheme(
+//             color: Colors.green,
+//           ),
+//         ),
+//         title: "test app",
+//         home: Scaffold(
+//           appBar: AppBar(
+//               backgroundColor: Colors.blue,
+//               // leading: const Icon(Icons.ios_share_outlined),
+//               // actions: const [Icon(Icons.control_camera_rounded)],
+
+//               title: const Center(child: Text("My First App"))),
+//           body: Center(
+//               child: Container(
+//             width: 300,
+//             height: 300,
+//             decoration: BoxDecoration(
+//               // borderRadius: BorderRadius.circular(0),
+//               color: Colors.red,
+//               border: Border.all(
+//                 color: Colors.blue,
+//                 width: 20,
+//               ),
+//             ),
+
+//             margin: const EdgeInsets.only(top: 30),
+//             padding: const EdgeInsets.only(top: 100),
+
+//             // constraints: const BoxConstraints(
+//             //   minHeight: 30,
+//             //   minWidth: 30,
+//             //   maxHeight: 50,
+//             //   maxWidth: 50,
+//             // ),
+//             //alignment: Alignment.topRight,
+//             // color: Colors.amber,
+//             child: const Text(
+//               "ITC BOOTCAMP",
+//               textAlign: TextAlign.center,
+//               style: TextStyle(
+//                 color: Colors.black,
+//                 fontSize: 20,
+//                 // fontWeight: FontWeight.w600),
+//               ),
+//             ),
+//           )),
+//         ));
+//   }
+// }
+
+///////////////////3333333333333333333////////////
+///
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyWidget());
+// }
+
+// class MyWidget extends StatelessWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         theme: ThemeData(
+//           appBarTheme: const AppBarTheme(
+//             color: Colors.green,
+//           ),
+//         ),
+//         title: "test app",
+//         home: Scaffold(
+//           appBar: AppBar(
+//               backgroundColor: Colors.blue,
+//               title: const Center(child: Text("My First App"))),
+//           body: Center(
+//               child: Container(
+//             width: 300,
+//             height: 300,
+//             decoration: BoxDecoration(
+//               // borderRadius: BorderRadius.circular(0),
+//               color: Colors.blue,
+//               border: Border.all(
+//                 color: Colors.blue,
+//                 width: 40,
+//               ),
+//             ),
+
+//             margin: const EdgeInsets.only(top: 1),
+//             padding: const EdgeInsets.only(top: 200),
+
+//             // constraints: const BoxConstraints(
+//             //   minHeight: 30,
+//             //   minWidth: 30,
+//             //   maxHeight: 50,
+//             //   maxWidth: 50,
+//             // ),
+//             //alignment: Alignment.topRight,
+//             // color: Colors.amber,
+//             child: const Text(
+//               "This is container",
+//               textAlign: TextAlign.end,
+//               style: TextStyle(
+//                 color: Colors.redAccent,
+//                 fontSize: 20,
+
+//                 // fontWeight: FontWeight.w600),
+//               ),
+//             ),
+//           )),
+//         ));
+//   }
+// }
+////////////////444444444444///////////
+///
+///
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyWidget());
+// }
+
+// class MyWidget extends StatelessWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         theme: ThemeData(
+//           appBarTheme: const AppBarTheme(
+//             color: Colors.green,
+//           ),
+//         ),
+//         title: "test app",
+//         home: Scaffold(
+//           appBar: AppBar(
+//               backgroundColor: Colors.blue,
+//               title: const Center(child: Text("My First App"))),
+//           body: Center(
+//               child: Container(
+//             width: 300,
+//             height: 300,
+//             decoration: const BoxDecoration(
+//               color: Colors.blue,
+//             ),
+//             padding: const EdgeInsets.only(top: 270),
+//             child: const Text(
+//               "This is container",
+//               textAlign: TextAlign.right,
+//               style: TextStyle(
+//                 color: Colors.redAccent,
+//                 fontSize: 20,
+//               ),
+//             ),
+//           )),
+//         ));
+//   }
+// }
+
+///////////////55555555555555///////////////
+///
+///
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyWidget());
+// }
+
+// class MyWidget extends StatelessWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         appBarTheme: const AppBarTheme(
+//           color: Colors.green,
+//         ),
+//       ),
+//       title: "test app",
+//       home: Scaffold(
+//           appBar: AppBar(
+//               backgroundColor: Colors.green,
+//               title: const Center(child: Text("My First App"))),
+//           body: Center(
+//             child: Container(
+//               width: 300,
+//               height: 300,
+//               decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(45),
+//                 color: Colors.blue,
+//               ),
+//               margin: const EdgeInsets.only(top: 40),
+//               padding: const EdgeInsets.only(top: 140),
+//               child: const Text(
+//                 "My name is Esentur",
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                     color: Colors.redAccent,
+//                     fontSize: 33,
+//                     fontWeight: FontWeight.w600),
+//               ),
+//             ),
+//           )),
+//     );
+//   }
+// }
+
+/////////////////////666666666666666///////////
+///
+///
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            color: Colors.green,
+          ),
+        ),
+        title: "test app",
+        home: Scaffold(
+          appBar: AppBar(
+              backgroundColor: Colors.blue,
+              title: const Center(child: Text("My First App"))),
+          body: Center(
+              child: Container(
+            width: 300,
+            height: 300,
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+            ),
+            padding: const EdgeInsets.only(top: 270),
+            child: const Text(
+              "This is container",
+              textAlign: TextAlign.right,
+              style: TextStyle(
+                color: Colors.redAccent,
+                fontSize: 20,
+              ),
+            ),
+          )),
+        ));
+  }
+}
